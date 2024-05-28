b17d6b6ea2a58c7d09630c221195c8baf3bb2b79
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2024-05-28 15:22:41 +0700

    create get user create-user login and logout

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +25,46 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::resource('/post', PostController::class);
 Route::resource('/user', UserController::class);
+
+Route::get('/get-user', function(Request $request){
+    $Users = UserResource::collection(User::all());
+    return response()->json(['data'=> $Users,'massage' => 'Request is successfully'], 200);
+});
+
+Route::post('/create-user', function (Request $request){
+    $request->validate([
+        'name'=> 'sometimes|required|string|max:255',
+        'email'=> 'sometimes|required|string',
+        'password'=> 'sometimes|required|string',
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+    ]);
+    return new UserResource($user);
+});
+
+Route::post('/login', function (Request $request){
+    $request->validate([
+        'email'=> 'sometimes|required|string',
+        'password'=> 'sometimes|required|string',
+    ]);
+
+    $credentials = request(['email', 'password']);
+
+    if (Auth::attempt($credentials)) {
+        $user = Auth::user();
+        $token = $user->createToken('MyApp')->plainTextToken;
+        return response()->json(['Login'=> $credentials,'massage' => 'login is successfully'], 200);
+    } else {
+        return response()->json(['error'=>'Unauthorised'], 400);
+    }
+});
+
+Route::middleware('auth:sanctum')->post('/logout', function(Request $request){
+    $request->user()->tokens()->delete();
+    return response()->json(['message' => 'Successfully logged out']);
+});
+
